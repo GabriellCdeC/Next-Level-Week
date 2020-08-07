c6ac223cb7213885386202ea5617fc10c43fb5fa
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2020-08-07 11:15:51 -0300

    update pages.js & server.js

--- src/server.js
+++ src/server.js
@@ -1,29 +1,7 @@
-const { subjects, weekdays, getSubject} = require('./utils/formart')
-
-function pageLanding (req, res) {
-    return res.render("index.html")
-}
-
-function pageGiveClasses (req, res) {
-    const data = req.query
-    const isNotEmpty = Object.keys(data).length > 0
-     //Adicionar data ao objeto de proffys
-    if(isNotEmpty){
-        data.subject = getSubject(data.subject)
-        proffys.push(data)
-        return res.redirect("/study")
-    }else {
-        return res.render("give-classes.html", {subjects, weekdays })
-    }     
-}
-
-function pageStudy (req, res) {
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
+//Servidor
 const express = require('express')
 const server = express()
+const { pageLanding, pageStudy, pageGiveClasses} = require('./pages')
 
 //Configurar o nunjucks (template engine)
 const nunjucks = require('nunjucks')
